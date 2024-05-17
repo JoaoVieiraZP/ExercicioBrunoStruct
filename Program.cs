4b46d416c6d693cd497e9fb30e24bf5fb2607c1c
JoaoVieiraZP <joaopedrovieirapereira5@gmail.com>
2024-05-17 14:54:13 -0300

    //Exercício Bruno C# 3

--- Program.cs
+++ Program.cs
@@ -13,7 +13,20 @@ namespace TAD{
             jogador1.adicionarCartaoAmarelo();
             jogador1.adicionarCartaoVermelho();
             jogador1.verificarVinculoClube();
-            Console.WriteLine(jogador1.imprimir());            
+            Console.WriteLine(jogador1.imprimir());      
+
+            Produto produto1 = new();
+
+            produto1.nome = "Bolacha";
+            produto1.valor = 2.50;
+            produto1.DescontoValor = 1;
+            produto1.DescontoPorcentagem = 15;
+            produto1.quantidade = 5;
+
+            produto1.aplicarCupomDescontoPorcentagem();
+            produto1.aplicarCupomDescontoValor();
+            produto1.verificarQuantidadeEmEstoque();
+            Console.WriteLine(produto1.imprimir());      
         }
     }   
 }
---
+++ struct/Produto.cs
@@ -0,0 +1,41 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace Struct;
+
+public struct Produto{
+    public string nome;
+
+    public double valor;
+
+    public int DescontoValor;
+
+    public int quantidade;
+
+    public double DescontoPorcentagem;
+
+    public Produto (string nome, double valor, int DescontoValor,  int quantidade, double DescontoPorcentagem){
+        this.nome = nome;
+        this.valor = valor;
+        this.DescontoValor = DescontoValor;
+        this.quantidade = quantidade;
+        this.DescontoPorcentagem= DescontoPorcentagem;
+    }
+
+    public string imprimir(){
+        return "\n Nome: " + this.nome + "\n Quantidade: " + this.quantidade + "\n Valor: R$" + string.Format("{0:0.00}", this.valor);
+    }
+
+    public void aplicarCupomDescontoValor(){
+        this.valor = valor - DescontoValor;
+    }
+
+    public void aplicarCupomDescontoPorcentagem(){
+        double desconto =  this.valor * (DescontoPorcentagem / 100);
+        this.valor -= desconto;
+    }
+
+    public int verificarQuantidadeEmEstoque(){
+        return this.quantidade;
+    }
+}
